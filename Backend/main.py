1791272a2bff6e595d8ab01605700d7b6c03c149
Alimddar <alimdarrmusayev9789@gmail.com>
2023-09-30 21:17:32 +0400

    User Crud🏁

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,5 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlmodel import Field, SQLModel, Session, create_engine
+from fastapi import Depends, FastAPI, HTTPException,status
+from sqlmodel import SQLModel, Session, create_engine,select
 from fastapi.middleware.cors import CORSMiddleware
 from models import User,WhiteBoard,Note,Schedule
 
@@ -16,3 +16,44 @@ def get_db():
     with Session(engine) as db:
         yield db
 
+@app.post('/users/', response_model=User, tags=['Users'])
+async def create_user(user: User, db: Session = Depends(get_db)):
+    statement = select(User).where(User.email == user.email)
+    db_user = db.exec(statement).first()
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return 'User Created succsessfully'
+
+@app.get("/users/{user_id}", response_model=User, tags=['Users'])
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
+
+@app.put("/users/{user_id}", response_model=User, tags=['Users'])
+async def update_user(user_id: int, user: User, db: Session = Depends(get_db)):
+    db_user = db.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    user_data = user.dict(exclude_unset=True)
+    for key, value in user_data.items():
+        setattr(db_user, key, value)
+    
+    db.commit()
+    db.refresh(db_user)
+    return 'User Updated succsessfully'
+
+@app.delete("/users/{user_id}", response_model=User, tags=['Users'])
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    db.delete(db_user)
+    db.commit()
+    return 'User Removed Succsessfully'
